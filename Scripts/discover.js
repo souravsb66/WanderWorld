76ad87085cabe82343ac3888da5cc366fd79576a
Srinivas8310 <hsrinivas802@gmail.com>
2023-07-19 22:18:01 +0530

    day-2 done

---
+++ Scripts/discover.js
@@ -0,0 +1,130 @@
+let arr=[
+    {
+        image:"https://adventor.wpengine.com/wp-content/uploads/2020/06/blog-010.jpg",
+        title:"Safety measures for safe trekking in waterfalls",
+       description:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, sunt veniam aperiam minus consequuntur autem quam culpa in quia sed? Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore magnam doloremque quis quia! Qui officia quaerat, praesentium voluptates maiores saepe molestiae quos quas cumque fugiat aut vitae assumenda perspiciatis itaque."
+    },
+    {
+        image:"https://adventor.wpengine.com/wp-content/uploads/2020/06/blog-006.jpg",
+        title:"How to select perfect quality camping tent",
+       description:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, sunt veniam aperiam minus consequuntur autem quam culpa in quia sed? Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore magnam doloremque quis quia! Qui officia quaerat, praesentium voluptates maiores saepe molestiae quos quas cumque fugiat aut vitae assumenda perspiciatis itaque."
+    },
+    {
+        image:"https://adventor.wpengine.com/wp-content/uploads/2020/06/blog-008-768x569.jpg",
+        title:"Happiness of camping with your loved once",
+       description:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, sunt veniam aperiam minus consequuntur autem quam culpa in quia sed? Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore magnam doloremque quis quia! Qui officia quaerat, praesentium voluptates maiores saepe molestiae quos quas cumque fugiat aut vitae assumenda perspiciatis itaque."
+    },
+    {
+        image:"https://adventor.wpengine.com/wp-content/uploads/2020/06/blog-009-768x569.jpg",
+        title:"Trekking spots in streams, waterfalls & rock cut trails",
+       description:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, sunt veniam aperiam minus consequuntur autem quam culpa in quia sed Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore magnam doloremque quis quia! Qui officia quaerat, praesentium voluptates maiores saepe molestiae quos quas cumque fugiat aut vitae assumenda perspiciatis itaque."
+    },
+    {
+        image:"https://adventor.wpengine.com/wp-content/uploads/2020/06/blog-001-768x569.jpg",
+        title:"Camping trails with hand-curated trail maps",
+       description:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, sunt veniam aperiam minus consequuntur autem quam culpa in quia sed? Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore magnam doloremque quis quia! Qui officia quaerat, praesentium voluptates maiores saepe molestiae quos quas cumque fugiat aut vitae assumenda perspiciatis itaque."
+    },
+    {
+        image:"https://adventor.wpengine.com/wp-content/uploads/2020/06/blog-002-768x569.jpg",
+        title:"Trekking & camping on hill top and mountains",
+       description:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, sunt veniam aperiam minus consequuntur autem quam culpa in quia sed? Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore magnam doloremque quis quia! Qui officia quaerat, praesentium voluptates maiores saepe molestiae quos quas cumque fugiat aut vitae assumenda perspiciatis itaque."
+    },
+    {
+        image:"https://adventor.wpengine.com/wp-content/uploads/2020/06/blog-003-768x569.jpg",
+        title:"Benefits & fun activities done during camping",
+       description:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, sunt veniam aperiam minus consequuntur autem quam culpa in quia sed? Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore magnam doloremque quis quia! Qui officia quaerat, praesentium voluptates maiores saepe molestiae quos quas cumque fugiat aut vitae assumenda perspiciatis itaque."
+    },
+    {
+        image:"https://adventor.wpengine.com/wp-content/uploads/2020/06/blog-004-768x569.jpg",
+        title:"Relaxing thing to do in trekking & camping",
+       description:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, sunt veniam aperiam minus consequuntur autem quam culpa in quia sed? Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore magnam doloremque quis quia! Qui officia quaerat, praesentium voluptates maiores saepe molestiae quos quas cumque fugiat aut vitae assumenda perspiciatis itaque."
+    },
+    {
+        image:"https://adventor.wpengine.com/wp-content/uploads/2020/06/blog-005-768x569.jpg",
+        title:"Great outdoor adventure photography",
+       description:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, sunt veniam aperiam minus consequuntur autem quam culpa in quia sed? Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore magnam doloremque quis quia! Qui officia quaerat, praesentium voluptates maiores saepe molestiae quos quas cumque fugiat aut vitae assumenda perspiciatis itaque."
+    },
+    {
+        image:"https://adventor.wpengine.com/wp-content/uploads/2020/06/blog-006.jpg",
+        title:"How to select perfect quality camping tent",
+       description:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, sunt veniam aperiam minus consequuntur autem quam culpa in quia sed? Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore magnam doloremque quis quia! Qui officia quaerat, praesentium voluptates maiores saepe molestiae quos quas cumque fugiat aut vitae assumenda perspiciatis itaque."
+    },
+    {
+        image:"https://adventor.wpengine.com/wp-content/uploads/2020/06/blog-004-768x569.jpg",
+        title:"Relaxing thing to do in trekking & camping",
+       description:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, sunt veniam aperiam minus consequuntur autem quam culpa in quia sed? Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore magnam doloremque quis quia! Qui officia quaerat, praesentium voluptates maiores saepe molestiae quos quas cumque fugiat aut vitae assumenda perspiciatis itaque."
+    },
+    {
+        image:"https://adventor.wpengine.com/wp-content/uploads/2020/06/blog-008-768x569.jpg",
+        title:"Happiness of camping with your loved once",
+       description:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, sunt veniam aperiam minus consequuntur autem quam culpa in quia sed? Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore magnam doloremque quis quia! Qui officia quaerat, praesentium voluptates maiores saepe molestiae quos quas cumque fugiat aut vitae assumenda perspiciatis itaque."
+    },
+    {
+        image:"https://adventor.wpengine.com/wp-content/uploads/2020/06/blog-009-768x569.jpg",
+        title:"Trekking spots in streams, waterfalls & rock cut trails",
+       description:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, sunt veniam aperiam minus consequuntur autem quam culpa in quia sed Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore magnam doloremque quis quia! Qui officia quaerat, praesentium voluptates maiores saepe molestiae quos quas cumque fugiat aut vitae assumenda perspiciatis itaque."
+    },
+    {
+        image:"https://adventor.wpengine.com/wp-content/uploads/2020/06/blog-001-768x569.jpg",
+        title:"Camping trails with hand-curated trail maps",
+       description:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, sunt veniam aperiam minus consequuntur autem quam culpa in quia sed? Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore magnam doloremque quis quia! Qui officia quaerat, praesentium voluptates maiores saepe molestiae quos quas cumque fugiat aut vitae assumenda perspiciatis itaque."
+    }
+    ]
+    
+    let pagiContainer=document.getElementById("pagiContainer");
+    
+    let cardlist=document.createElement("div");
+    cardlist.classList.add("cardlist");
+    let pagination=document.querySelector("#pagination");
+
+    let itemPerPage=8;
+    function fetching(){
+        let length=arr.length;
+        let total=Math.ceil(length/itemPerPage);
+        display(1);
+ 
+        for(let i=1;i<=total;i++){
+            let pagiButton=document.createElement("button");
+            pagiButton.innerText=i;
+            pagiButton.classList.add("pagination-button");
+            pagiButton.addEventListener("click",function(){
+                display(i);
+            });
+            pagination.append(pagiButton);
+        }
+    }
+    fetching();
+
+    function display(page){
+        cardlist.innerHTML="";
+
+        let start=(page-1)* itemPerPage;
+        let end=start+itemPerPage;
+        let modifiedArr=arr.slice(start,end);
+
+        modifiedArr.forEach(function(ele){
+        let card=document.createElement("div");
+        card.classList.add("card");
+
+        let imageDiv=document.createElement("div");
+        imageDiv.classList.add("imageDiv");
+        let image=document.createElement("img");
+        image.setAttribute("src",ele.image);
+
+        let title=document.createElement("h1");
+        title.classList.add("h1");
+        title.innerText=ele.title;
+
+        let description=document.createElement("p");
+       description.innerText=ele.description;
+       
+       let readMore=document.createElement("button");
+       readMore.classList.add("readMore");
+       readMore.innerText="SEE MORE";
+       imageDiv.append(image);
+       card.append(imageDiv,title,description,readMore);
+       cardlist.append(card);
+    })
+    pagiContainer.innerHTML="";
+    pagiContainer.append(cardlist);
+    }
