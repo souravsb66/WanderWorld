78fd026e54b589e69473559480624a6f63baff8a
Ankit Kumar <ankkrs@gmail.com>
2023-07-23 11:53:49 +0530

    day-5 work

---
+++ Scripts/confo.js
@@ -0,0 +1,70 @@
+const container = document.querySelector('.container-fluid');
+const starsDiv = document.querySelector('#stars');
+const canvas = document.createElement('canvas');
+canvas.width = container.offsetWidth;
+canvas.height = container.offsetHeight;
+container.appendChild(canvas);
+const ctx = canvas.getContext('2d');
+let stars = [];
+class Star {
+    constructor(x, y, color) {
+        this.x = x;
+        this.y = y;
+        this.radius = 2;
+        this.color = color;
+    }
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
+    }
+    update() {
+        this.y--;
+        this.draw();
+    }
+}
+function generateStar(x) {
+    let color;
+    let random = Math.random();
+    if (random < 0.5) {
+        color = '#ff0';
+    } else {
+        color = '#000';
+    }
+    let star = new Star(x, canvas.height, color);
+    stars.push(star);
+}
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    stars.forEach((star, index) => {
+        star.update();
+        if (star.y <= 0) {
+            stars.splice(index, 1);
+        }
+    });
+    let x = Math.floor(Math.random() * (canvas.width + 1));
+    generateStar(x);
+    requestAnimationFrame(animate);
+}
+animate();
+
+
+// 
+
+
+let re_time = document.querySelector("#re-time");
+
+let i = 15;
+
+
+
+let set = setInterval(() => {
+    i--;
+    re_time.innerText = i;
+    if (i == 0) {
+        window.location = "";
+        clearInterval(set);
+    }
+}, 1000);
--- Scripts/package.js
+++ Scripts/package.js
@@ -19,22 +19,60 @@
    const popup = document.querySelector('.popup');
    const closeButton = document.querySelector('.close-button');
    
-   const body_parent = document.querySelector(".row")
+   const body_parent = document.querySelector(".row");
+
+   const body = document.querySelector(".body-parent");
+
+   let food = document.querySelector("#food");
+   let hotel = document.querySelector("#hotel");
+
+   let dark_details = document.querySelector("#dark_details");
+   let side_details = document.querySelector("#side_details");
+
+   
 
 
    // Show the pop-up when the button is clicked
    button.addEventListener('click', () => {
+    hotel.innerText = "3 Star Hotel";
+    food.innerText = "Paid Food"
+    body.style.backgroundColor = "rgb(215, 222, 229) "
      popup.style.display = 'block';
     //  body_parent.style.display ="none";
     body_parent.setAttribute("z-index","-1");
-    popup.style.zIndex="9"
+    popup.style.zIndex="9";
+
    });
 
    // Hide the pop-up when the close button is clicked
    closeButton.addEventListener('click', () => {
+    body.style.backgroundColor = "";
      popup.style.display = 'none';
      body_parent.style.display ="flex"
    });
+
+   dark_details.addEventListener('click', () => {
+    hotel.innerText = "4 Star Hotel";
+    food.innerText = "Free Food";
+    body.style.backgroundColor = "rgb(215, 222, 229) "
+     popup.style.display = 'block';
+    //  body_parent.style.display ="none";
+    body_parent.setAttribute("z-index","-1");
+    popup.style.zIndex="9";
+
+   });
+
+   side_details.addEventListener('click', () => {
+    hotel.innerText = "5 Star Hotel";
+    food.innerText = "Free Food";
+    body.style.backgroundColor = "rgb(215, 222, 229) "
+     popup.style.display = 'block';
+    //  body_parent.style.display ="none";
+    body_parent.setAttribute("z-index","-1");
+    popup.style.zIndex="9";
+
+   });
+
 
 
 
@@ -54,8 +92,9 @@
     
     let int = setInterval(()=>{
       slogan_text+= str[i];
-       console.log(str[i]);
+      //  console.log(str[i]);
        package_slogan.innerText = slogan_text;
+      //  package_slogan.className = "text-white"
        i++
        if(i == str.length){
         i=0
@@ -67,7 +106,7 @@
 
 display();
 
-// setInterval(display,11000)
+setInterval(display,11000)
 
 
 // card
@@ -76,35 +115,68 @@ let first_card = document.querySelector(".first-card");
 let dark_card = document.querySelector(".dark_card");
 let side_card = document.querySelector(".side-card");
 
+let plan_price_end = document.querySelector(".plan-price-end");
+
+let screenWidth = window.screen.width;
+
+if(screenWidth<900){
+  first_card.style.position= "static";
+  dark_card.style.position= "static";
+  side_card.position= "static";
+}else{
+  first_card.style.position= "relative";
+  dark_card.style.position= "absolute";
+  side_card.position= "relative";
+  first_card.style.left ="-5%";
+  side_card.style.right ="-3%"
+  plan_price_end.style.float = "right";
+}
+
+if(screenWidth>=600){
+
+
+
 first_card.addEventListener("mouseover",()=>{
-  // first_card.style.left ="0"
-  dark_card .style.position = "static";
+  // dark_card .style.position = "static";
   dark_card .style.zIndex ="-1";
   side_card.style.zIndex ="-2";
-  side_card.style.right ="9%"
-  first_card.style.left ="9%";
+  // side_card.style.right ="9%"
+  // first_card.style.left ="9%";
 
-  first_card.style.width ="400px";
-  // first_card.style.zIndex ="2"
+  // first_card.style.width ="350px";
 })
 
 first_card.addEventListener("mouseout",()=>{
   dark_card .style.position = "absolute";
-  dark_card .style.zIndex ="1";
+  dark_card .style.zIndex ="2";
+  side_card.style.zIndex ="1";
   side_card.style.position = "relative";
   first_card.style.left ="-5%";
-  side_card.style.right ="0%"
+  side_card.style.right ="-3%"
 
   first_card.style.width ="300px";
-  // first_card.style.zIndex ="-1"
+
 })
 
 
 
 side_card.addEventListener("mouseover",()=>{
-  dark_card.style.zIndex="-1";
-  side_card.style.zIndex="2"
+first_card.style.zIndex -"-1";
+  dark_card.style.zIndex="1";
+  side_card.style.zIndex="2";
+  side_card.style.width ="350px";
+  plan_price_end.style.float = "left"
+});
+
+
+side_card.addEventListener("mouseout",()=>{
+// first_card.style.zIndex -"1";
+  dark_card.style.zIndex="1";
+  side_card.style.zIndex="-1";
+  side_card.style.width ="350px";
+  plan_price_end.style.float = "right"
 })
 
 
+}
 
